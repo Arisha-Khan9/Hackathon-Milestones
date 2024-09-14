f27d477005a174f456e3d0ea864ba8544d24c491
arisha khan <160261470+Arisha-Khan9@users.noreply.github.com>
2024-09-15 00:38:59 +0500

    Add files via upload

---
+++ Milestone-4/script.ts
@@ -0,0 +1,40 @@
+// import form from HTML
+
+//HANDLE FORM SUBMISSION
+(document.getElementById('resume-form') as HTMLFormElement).addEventListener ('submit',(event: Event) => {
+event.preventDefault();     // prevent page reload
+
+// collect input values
+const name = (document.getElementById('name') as HTMLInputElement).value
+const email = (document.getElementById('email') as HTMLInputElement).value
+const phone = (document.getElementById('phone') as HTMLInputElement).value
+const education = (document.getElementById('education') as HTMLInputElement).value
+const experience = (document.getElementById('experience') as HTMLInputElement).value
+const skills = (document.getElementById('skills') as HTMLInputElement).value
+
+// GENERATE THE RESUME CONTENT DYNAMICALLY
+const resumeHTML = `
+<h2><b> Editable Resume </b></h2>
+<h3> Personal Information </h3>
+
+<p><b> Name: </b> <span contenteditable = "true"> ${name} </span> </p>
+<p><b> Email: </b> <span contenteditable = "true"> ${email} </span> </p>
+<p><b> Phone: </b> <span contenteditable = "true"> ${phone} </span> </p>
+
+<h3> Education </h3>
+<p contenteditable = "true"> ${education}</p>
+
+<h3> Experience </h3>
+<p contenteditable = "true"> ${experience}</p>
+
+<h3> Skills </h3>
+<p contenteditable = "true"> ${skills}</p>`;
+
+
+//display the generated resume
+if (document.getElementById('resume-display') as HTMLDivElement) {
+    (document.getElementById('resume-display') as HTMLDivElement).innerHTML = resumeHTML
+}else {
+    console.error('The Resume Display Element Is Missing.')
+}
+});
